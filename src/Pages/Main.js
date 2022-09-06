83c4c9d4e598d59c7f1162f85032d6eceed03baa
Meth Munindradasa <methsndr@gmail.com>
2022-09-06 10:20:48 +0530

    Get items from the backend

--- src/Pages/Main.js
+++ src/Pages/Main.js
@@ -4,34 +4,48 @@ import ProductListItem from "../Components/Minor/ProductListItem";
 import SubHeader from "../Components/SubHeader";
 
 import Product1 from "../assets/images/product-img-1.png";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 
 function Main(){
 	
-	let items = [
-		<ProductListItem sku="#CA25"
-										 img={Product1}
-										 product_name="Product-name"
-										 price="$24.00"/>,
-		<ProductListItem sku="#CA25"
-										 img={Product1}
-										 product_name="Product-name"
-										 price="$24.00"/>,
-		<ProductListItem sku="#CA25"
-										 img={Product1}
-										 product_name="Product-name"
-										 price="$24.00"/>,
-		<ProductListItem sku="#CA25"
-										 img={Product1}
-										 product_name="Product-name"
-										 price="$24.00"/>,
-	]
+	// let items = [
+	// 	<ProductListItem sku="#CA25"
+	// 									 img={Product1}
+	// 									 product_name="Product-name"
+	// 									 price="$24.00"/>,
+	// 	<ProductListItem sku="#CA25"
+	// 									 img={Product1}
+	// 									 product_name="Product-name"
+	// 									 price="$24.00"/>,
+	// 	<ProductListItem sku="#CA25"
+	// 									 img={Product1}
+	// 									 product_name="Product-name"
+	// 									 price="$24.00"/>,
+	// 	<ProductListItem sku="#CA25"
+	// 									 img={Product1}
+	// 									 product_name="Product-name"
+	// 									 price="$24.00"/>,
+	// ]
+
+	const [items, setItems] = useState([]);
 	
 	useEffect(()=>{
 		console.log("running use effect");
-		axios.get('http://localhost:8080')
-				 .then(resp=>console.log(resp))
+		axios.get('http://localhost:8080/products')
+				 .then(resp=>{
+						let allData = [];
+						resp.data.forEach(item => {
+							allData.push(<ProductListItem 
+																						sku={item["SKU"]}
+																						img={""}
+																						product_name={item["Name"]}
+																						price={item["QTY"]}
+																						key={item["SKU"]}
+																						/>);
+						});
+						setItems(allData);
+				 })
 				 .catch(e=>console.log(e));
 	}, []);
 	
