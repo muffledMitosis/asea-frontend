2ba22b7b4b6482a02e68a03dd68023e51c342a43
Meth Munindradasa <methsndr@gmail.com>
2022-09-06 14:56:27 +0530

    Add user icon in page header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,14 +1,20 @@
+import {VscTriangleDown} from 'react-icons/vsc';
+
 function Header(){
 	return (
-		<div className="flex flex-row justify-end">
-			<div className="flex">
+		<div className="flex flex-row justify-end items-center p-4">
+			<div className="flex justify-center align-baseline">
 				<div className="font-bold">
 					ADMIN
 				</div>
-				<div className="font-bold">
+				<div className="ml-2 font-bold pt-1">
+					<VscTriangleDown />
 				</div>
 			</div>
-			{/* <div>IMG</div> */}
+			<div className='w-10 h-10 rounded-full bg-t-blue ml-4'>
+				<div className='relative w-3 h-3 rounded-full bg-green-400 top-7 left-7'>
+				</div>
+			</div>
 		</div>
 	);
 }
